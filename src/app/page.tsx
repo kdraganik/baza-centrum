b97df7f60f6fb9130622008c473a546ccc67f005
Karol Draganik <karol.draganik@gmail.com>
2026-08-26 12:51:16 +0200

    Add logo and date placehoder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Baza Centrum</h1>
-    </div>
+    <main className={styles.page}>
+      <Image
+        className={styles.background}
+        src="/baza-bg.png"
+        alt=""
+        fill
+        priority
+        sizes="100vw"
+      />
+
+      <div className={styles.event}>
+        <Image
+          className={styles.title}
+          src="/title.png"
+          alt="Baza Centrum"
+          width={1138}
+          height={161}
+          priority
+        />
+        <Image
+          className={styles.date}
+          src="/date.png"
+          alt=""
+          width={1091}
+          height={67}
+          priority
+        />
+      </div>
+
+      <Image
+        className={styles.logo}
+        src="/logo.png"
+        alt="Baza"
+        width={727}
+        height={288}
+        priority
+      />
+    </main>
   );
 }
